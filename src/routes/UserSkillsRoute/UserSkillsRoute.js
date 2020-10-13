682b95b139d481b271c2d0f1bbdbb89122eaa3d1
Sonali Najera <sonalitzul@gmail.com>
2020-10-13 12:45:30 -0700

    added user profile skills route and component

--- src/routes/UserSkillsRoute/UserSkillsRoute.js
+++ src/routes/UserSkillsRoute/UserSkillsRoute.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ProfilePicture from '../../images/user-placeholder.jpg';
 
 export class SkillCardRoute extends Component {
   render() {
     return (
       <div>
         <h2>Username's Profile and Skills they offer</h2>
-        <section className='user-info-section'> 
-        
+        <section className='user-info-section' style={{border: '2px solid black', width: '80%', margin: 'auto'}}> 
+        <img style={{maxWidth: 60}} src={ProfilePicture} alt='user profile picture' className='userProfilePhoto'></img>
+        <p>Username</p>
+        <p>Bio if provided</p>
         </section>
       </div>
     )
