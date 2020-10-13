700da7994a810fbf4a9e2a3687d79aedde16ed4f
Lucas Bigelow <lukejbigelow@gmail.com>
2020-10-13 15:53:57 -0500

    Component/header (#5)
    * WIP header
    
    * WIP finishing header wireframe
    
    * WIP initial header wireframe

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import HamburgerMenu from 'react-hamburger-menu';
 import TokenService from '../../services/token-service';
 import UserContext from '../../contexts/UserContext';
 import './Header.css';
@@ -7,15 +8,48 @@ import './Header.css';
 export default class Header extends Component {
   static contextType = UserContext;
 
+  state = {
+    menuOpen: false,
+  };
+
+  handleMenuClick  = () => {
+    this.setState({
+      menuOpen: !this.state.menuOpen
+    });
+  };
+
   handleLogoutClick = () => {
     this.context.processLogout();
   };
 
   render() {
     return (
-      <h1>
-        Burnt Toast
-      </h1>
-    )
-  }
+      <header className="Header-group">
+        <div id="logo" className="Header-item">
+          <img src="#" alt="PLACEHOLDER TEXT" />
+        </div>
+        <div id="username-and-menu" className="menu-group">
+          <p id="username" className="menu-item">{this.context.user.name}username</p>
+          <nav className="menu-item">
+            <HamburgerMenu 
+              isOpen={this.state.menuOpen}
+              menuClicked={this.handleMenuClick.bind(this)}
+            />
+            {this.state.menuOpen && <ul id="dropdown-menu">
+              <li>              
+                <Link to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to={`/profiles/${this.context.user.id}`}>
+                  Profile
+                </Link>
+              </li>
+            </ul>}
+          </nav>
+        </div>
+      </header>
+    );
+  };
 }
