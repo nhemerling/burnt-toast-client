9c3a5b7492b2c14ca9219cc94c864a9823d44273
Sonali Najera <sonalitzul@gmail.com>
2020-10-13 12:33:15 -0700

    user skills view

---
+++ src/routes/UserSkillsRoute/UserSkillsRoute.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class SkillCardRoute extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Username's Profile and Skills they offer</h2>
+        <section className='user-info-section'> 
+        
+        </section>
+      </div>
+    )
+  }
+}
+
+export default SkillCardRoute
