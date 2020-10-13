6d4ee44697e1cb90c42a5a2570dbb27ff5fc5fcf
Lucas Bigelow <lukejbigelow@gmail.com>
2020-10-13 13:21:25 -0500

    WIP header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import HamburgerMenu from 'react-hamburger-menu';
 import TokenService from '../../services/token-service';
 import UserContext from '../../contexts/UserContext';
 import './Header.css';
@@ -7,15 +8,36 @@ import './Header.css';
 export default class Header extends Component {
   static contextType = UserContext;
 
+  state = {
+    menuOpen: false,
+  };
+
+  handleMenuClick  = () => {
+    this.setState({
+      menuOpen: !this.state.menuOpen
+    });
+  };
+
   handleLogoutClick = () => {
     this.context.processLogout();
   };
 
   render() {
     return (
-      <h1>
-        Burnt Toast
-      </h1>
-    )
-  }
+      <header class="Header-group">
+        <div id="logo" class="Header-item">
+          <img src="#" alt="PLACEHOLDER TEXT" />
+        </div>
+        <div id="username-and-menu" class="Header-itme menu-group">
+          <p id="username" class="menu-item">{this.context.user.name}USERNAMEEEEE</p>
+          <nav class="menu-item">
+            <HamburgerMenu 
+              isOpen={this.state.menuOpen}
+              menuClicked={this.handleMenuClick.bind(this)}
+            />
+          </nav>
+        </div>
+      </header>
+    );
+  };
 }
