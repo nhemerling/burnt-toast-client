3b008c3ac4663902b3ce75d6acbb6b4b4bae174f
nhemerling <65194792+nhemerling@users.noreply.github.com>
2020-10-16 12:16:41 -0700

    Feature/search page (#18)
    * resolve conflicts
    
    * merge conflicts
    
    * reorganize route switch and give list items keys
    
    * WIP Search bar
    
    * WIP search bar with select
    
    * add .vercel
    
    * add burnt toast api service
    
    * add BurntToastContext with categories and services and added a BurntToastService fetch endpoint
    
    * implement CategorySelect component and WIP SearchRoute
    
    * implement CategorySelect component and WIP SearchRoute
    
    * implemented CategorySelect component and integrate it with SearchRoute
    
    * implement search fetch request and make service cards links to user service route
    
    Co-authored-by: Lucas Bigelow <lukejbigelow@gmail.com>

--- src/components/ServiceCard/ServiceCard.js
+++ src/components/ServiceCard/ServiceCard.js
@@ -4,12 +4,11 @@ import './ServiceCard.css'
 class ServiceCard extends Component {
   render() {
     return(
-      <div className="ServiceCard">
-        <img src="" alt={this.props.category} />
-        {/* <p>{this.props.user.full_name}</p> */}
-        <p>{this.props.service}</p>
-        <p>{this.props.description}</p>
-      </div>
+        <div className="ServiceCard" >
+          <img src="" alt={this.props.category} />
+          <p>{this.props.service}</p>
+          <p>{this.props.description}</p>
+        </div>
     );
   }
 }
