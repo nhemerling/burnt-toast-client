86519a5fd190339918bf1a70d46b530d42d9ca62
Lucas Bigelow <lukejbigelow@gmail.com>
2020-10-20 09:22:48 -0500

    WIP UserServiceRoute

--- src/components/ServiceCard/ServiceCard.js
+++ src/components/ServiceCard/ServiceCard.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './ServiceCard.css'
 
+// id={service.id}
+// user_id={service.fk_user_id}
+// service_id={service.fk_skill_id}
+// service={searchService}
+// type={service.user_skill_type}
+// image={service.primary_img_url}
+// description={service.primary_description}
+
 class ServiceCard extends Component {
   render() {
     return(
         <div className="ServiceCard" >
-          <img src="" alt={this.props.category} />
-          <p>{this.props.service}</p>
-          <p>{this.props.description}</p>
+          {this.props.image && <img src={this.props.image} alt="service provided image" />}
+          {this.props.service && <h3>{this.props.service}</h3>}
+          {this.props.description && <p>{this.props.description}</p>}
         </div>
     );
   }
--- src/routes/UserServiceRoute/UserServiceRoute.js
+++ src/routes/UserServiceRoute/UserServiceRoute.js
@@ -8,18 +8,24 @@ import BurntToastService from '../../services/burnt-toast-api-service';
 export class ServiceCardRoute extends Component {
   state = {
     user: {},
+    userId: 1,
     userServices: []
   };
 
   componentDidMount() {
-    this.setState({user: BurntToastService.getProfile(ID)});
-    this.setState({userServices: BurntToastService.getProfileServices(ID)});
+    let userId = this.props;
+    console.log(userId);
+    this.setState({user: BurntToastService.getProfile(this.props.match.params.profile_id).then(res => res)});
+    this.setState({userServices: BurntToastService.getProfileServices(this.props.match.params.profile_id).then(res => res)});
+    // BurntToastService.getProfile(userId).then(res => console.log(res));
+    // BurntToastService.getProfileServices(userId).then(res => res.json())
   }
 
   render() {
-    const services = STORE.serviceCards.cards;
+    const services = this.state.userServices;
+    console.log(services);
     // TODO: ONLY POPULATE IF USER ID === PROFILE VISIT ID 
-    const serviceList = services.map((services, i) => {
+    const serviceList = this.state.userServices.map((services, i) => {
       return (
         <div key={i} className='service'>
           <img src={ServiceHolder} alt='image with chart of different service categories' className='service-img'></img>
@@ -30,20 +36,40 @@ export class ServiceCardRoute extends Component {
       )
     })
 
-    return (
-      <div>
-        <h2>{this.state.user.username}'s Profile</h2>
-        <section className='user-info-section' style={{ border: '2px solid black', width: '80%', margin: 'auto' }}>
-          <img style={{ maxWidth: 60 }} src={ProfilePicture} alt='user profile picture' className='user-profile-photo'></img>
-          <p>{this.state.user.name}</p>
-          <p>{this.state.user.bio}</p>
-        </section>
+    console.log(this.state.user);
+    console.log(this.state.userId);
+    console.log(this.state.userServices);
 
-        {serviceList}
-      </div>
-    )
+    if (this.state.user) {
+      return (<div>
+      <h2>{this.state.user.username}'s Profile</h2>
+      <section className='user-info-section' style={{ border: '2px solid black', width: '80%', margin: 'auto' }}>
+        <img style={{ maxWidth: 60 }} src={ProfilePicture} alt='user profile picture' className='user-profile-photo'></img>
+        <p>{this.state.user.name}</p>
+        <p>{this.state.user.bio}</p>
+      </section>
+      {console.log(this.state.user + "AHHHHHHHHHH FUCK")}
+
+      {serviceList}
+    </div>);
+    } else {
+      return <h1>YOU DONE EFFED UP</h1>;
+    }
+
+    // return (
+    //   <div>
+    //     <h2>{this.state.user.username}'s Profile</h2>
+    //     <section className='user-info-section' style={{ border: '2px solid black', width: '80%', margin: 'auto' }}>
+    //       <img style={{ maxWidth: 60 }} src={ProfilePicture} alt='user profile picture' className='user-profile-photo'></img>
+    //       <p>{this.state.user.name}</p>
+    //       <p>{this.state.user.bio}</p>
+    //     </section>
+    //     {console.log(this.state.user + "AHHHHHHHHHH FUCK")}
+    //     {serviceList}
+    //   </div>
+    // )
   }
 }
 
-export default ServiceCardRoute
+export default ServiceCardRoute;
 
