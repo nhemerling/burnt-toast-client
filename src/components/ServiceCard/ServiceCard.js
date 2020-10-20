9a2984c367ee962173d96a8075f1692805570714
Sonali Najera <sonalitzul@gmail.com>
2020-10-20 09:12:51 -0700

    image placeholder

--- src/components/ServiceCard/ServiceCard.js
+++ src/components/ServiceCard/ServiceCard.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './ServiceCard.css'
+import ServiceHolder from '../../images/service-placeholder.png';
 
 class ServiceCard extends Component {
   render() {
     return(
         <div className="ServiceCard" >
+          <img src={ServiceHolder} className='service-img'></img>
           <p>Category: {this.props.category}</p>
           <p>Service: {this.props.service}</p>
           <p>Description: {this.props.description}</p>
