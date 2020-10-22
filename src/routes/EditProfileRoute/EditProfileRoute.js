4ef04aa1fd8208ad8571675afec4adfea5ad1fb8
Sonali Najera <sonalitzul@gmail.com>
2020-10-22 13:14:51 -0700

    added delete forms for skills and profile

--- src/routes/EditProfileRoute/EditProfileRoute.js
+++ src/routes/EditProfileRoute/EditProfileRoute.js
@@ -3,7 +3,7 @@ import ServiceOfferForm from '../../components/ServiceOfferForm/ServiceOfferForm
 import ServiceSeek from '../../components/ServiceSeek/ServiceSeek';
 import PersonalizeProfile from '../../components/PersonalizeProfileForm/PersonalizeProfileForm';
 import BurntToastService from '../../services/burnt-toast-api-service';
-
+import Button from '../../components/Button/Button';
 import './EditProfileRoute.css'
 import ProfileEditImg from '../../images/profile-edit.png'
 import BurntToastContext from '../../contexts/BurntToastContext';
@@ -11,7 +11,9 @@ import BurntToastContext from '../../contexts/BurntToastContext';
 export class EditProfileRoute extends Component {
 
   state = {
-    userServices: []
+    provided: [],
+    seeking: [],
+    deleteClicked: false
   }
 
   static contextType = BurntToastContext;
@@ -20,30 +22,113 @@ export class EditProfileRoute extends Component {
     this.context.getCategoriesAndServices();
 
     let currentUserId = this.props.match.params.profile_id;
-    // BurntToastService.getProfile(currentUserId).then(res => {
-    //   this.setState({user: res, userId: currentUserId});
-    // });
-    BurntToastService.getProfileServices(currentUserId).then(res => {
-      console.log(res)
-      // this.setState({userServices: res});
+
+    BurntToastService.getProfileServices(currentUserId).then(userServices => {
+      let provided = [];
+      let seeking = [];
+      for (let i = 0; i < userServices.length; i++) {
+        if (userServices[i].user_skill_type === 'PROVIDER') {
+          provided.push(userServices[i]);
+        } else {
+          seeking.push(userServices[i]);
+        }
+      }
+      this.setState({
+        provided: [...provided],
+        seeking: [...seeking]
+      });
     });
   }
 
+  handleDeleteClick = () => {
+    this.setState({
+      deleteClicked: true
+    });
+  };
+
+  handleDeleteConfirmation = () => {
+
+  };
+
+  handleKeepAccountClick = () => {
+    this.setState({
+      deleteClicked: false
+    })
+  }
+
+  renderDeleteConfirmation = () => {
+    return (
+    <div className='delete-confirmation'>
+      <p>You will not be able to recover your profile after deleting.</p>
+      <p style={{fontWeight:'bold'}}>PLEASE CONFIRM YOUR DELETE REQUEST</p>
+      <Button type='button' className='delete-button-keep' onClick={this.handleKeepAccountClick}>Keep account</Button>
+      <br/>
+      <Button type='button' className='delete-button' onClick={this.handleDeleteConfirmation}>Confirm Delete</Button>
+    </div>
+    );
+  };
+
   render() {
+    let serviceOffer = this.state.provided ? this.state.provided : [];
+    let serviceSeek = this.state.seeking ? this.state.seeking : [];
+
+    let offers = serviceOffer.map((offer, i) => (
+      <div key={i} className='user-card-item'>
+        <div>
+          <h3>{offer.skill_name}</h3>
+          <p>{offer.primary_description}</p>
+        </div>
+        <Button type='button' className='delete-service-item'>remove</Button>
+      </div>))
+
+    let seeks = serviceSeek.map((seek, i) => (
+      <div key={i} className='user-card-item'>
+        <div>
+          <h3>{seek.skill_name}</h3>
+        </div>
+        <Button type='button' className='delete-service-item'>remove</Button>
+      </div>))
+
+    let deleteButton = this.state.deleteClicked 
+      ? this.renderDeleteConfirmation() 
+      : (<Button type='button' className='delete-button' onClick={this.handleDeleteClick}> Delete Profile </Button>);
 
     return (
       <div>
-        <h2> Manage Profile</h2>
+        <h1> Manage Profile</h1>
         <img src={ProfileEditImg} className='manage-profile-img' alt=''></img>
         <section>
-          <ServiceOfferForm/>
+          <div className='user-service-card-display'>
+            <section className='service-provided'>
+              <h2>Services You're Providing</h2>
+              {offers}
+            </section>
+            <section className='service-seeking'>
+              <h2>Services You're Seeking</h2>
+              {seeks}
+            </section>
+          </div>
         </section>
+        <section>
+          <ServiceOfferForm />
+        </section>
+        <section>
+          <ServiceSeek />
+        </section>
+        <section>
+          <PersonalizeProfile />
+        </section>
+        <section className='user-delete-profile'>
+          <h2>Danger Zone</h2>
+          <p>Deleting profile will remove you from our records permanently.</p>
+          {/* <Button 
+          type='button'
+          onClick={this.handleDeleteClick}
+          >
+            Delete Profile
+          </Button> */}
 
-        <section>
-          <ServiceSeek/>
-        </section>
-        <section>
-        <PersonalizeProfile/>
+          {deleteButton}
         </section>
       </div>
     )
