23987617294845a48e58f2b4f90cadf3f3b19001
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-10-28 09:11:09 -0700

    fix bug with user service route id not found

--- src/routes/UserServiceRoute/UserServiceRoute.js
+++ src/routes/UserServiceRoute/UserServiceRoute.js
@@ -4,7 +4,7 @@ import BurntToastService from '../../services/burnt-toast-api-service';
 import ServiceCard from '../../components/ServiceCard/ServiceCard';
 import './UserServiceRoute.css'
 
-export class ServiceCardRoute extends Component {
+export class UserServiceRoute extends Component {
   static defaultProps = {
     match: {
       params: {
@@ -16,17 +16,27 @@ export class ServiceCardRoute extends Component {
   state = {
     user: {},
     userId: 1,
-    userServices: []
+    userServices: [],
+    profileError: null,
+    servicesError: null,
   };
 
   componentDidMount() {
     let currentUserId = this.props.match.params.profile_id;
-    BurntToastService.getProfile(currentUserId).then(res => {
-      this.setState({user: res, userId: currentUserId});
+    this.setState({
+      profileError: null,
+      servicesError: null,
     });
-    BurntToastService.getProfileServices(currentUserId).then(res => {
+    BurntToastService.getProfile(currentUserId)
+      .then(res => {
+        this.setState({user: res, userId: currentUserId});
+      })
+      .catch(error => this.setState({ profileError: error.error }));
+    BurntToastService.getProfileServices(currentUserId)
+      .then(res => {
       this.setState({userServices: res});
-    });
+      })
+      .catch(error => this.setState({ servicesError: error.error }));
   }
 
   render() {
@@ -42,24 +52,27 @@ export class ServiceCardRoute extends Component {
           />
       )
     })
-    if (this.state.user) {
+    if (this.state.user.id) {
       return (
-      <>
-      <section className='profile-info'>
-        <img src={this.state.user.profile_img_url ? this.state.user.profile_img_url : 'https://miro.medium.com/max/360/1*W35QUSvGpcLuxPo3SRTH4w.png'} alt='user profile avatar' className='user-profile-photo'></img>
-        <div>
-          <p>{this.state.user.profile_desc}</p>
-          <button type='submit'><a href={`mailto:${this.state.user.email}?subject=Burnt Toast: Inquiry About Your Profile`}>Contact</a></button>
-        </div>
-      </section>
-      <h3 id='user-name-services'>{this.state.user.full_name}'s Services</h3>
-      <div className='service-card-list'>
-        {serviceList}
-      </div>
-    </>
-    );}
+        <>
+          <section className='profile-info'>
+            <img src={this.state.user.profile_img_url ? this.state.user.profile_img_url : 'https://miro.medium.com/max/360/1*W35QUSvGpcLuxPo3SRTH4w.png'} alt='user profile avatar' className='user-profile-photo'></img>
+            <div>
+              <p>{this.state.user.profile_desc}</p>
+              <button type='submit'><a href={`mailto:${this.state.user.email}?subject=Burnt Toast: Inquiry About Your Profile`}>Contact</a></button>
+            </div>
+          </section>
+          <h3 id='user-name-services'>{this.state.user.full_name}'s Services</h3>
+          <div className='service-card-list'>
+            {serviceList}
+          </div>
+        </>
+      );
+    } else {
+      return <h3>{this.state.profileError}</h3>
+    }
   }
 }
 
-export default ServiceCardRoute;
+export default UserServiceRoute;
 
